2162d40c3bb20bab64c7236f3067bb3402e8d114
Jacob Sanford <jsanford@unb.ca>
2022-10-31 10:29:09 -0300

    Initial commit from prototype

---
+++ src/Robo/Plugin/Commands/MongodbDeploymentCommands.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dockworker\Robo\Plugin\Commands;
+
+use Dockworker\Robo\Plugin\Commands\DockworkerLocalCommands;
+
+/**
+ * Defines the commands used to interact with a local Mongodb application.
+ */
+class MongodbDeploymentCommands extends DockworkerLocalCommands {
+
+  /**
+   * Provides log checker with ignored log exception items for local Mongodb.
+   *
+   * @hook on-event dockworker-deployment-log-error-exceptions
+   */
+  public function getErrorLogExceptions() {
+    return [
+        'config.system.sessions does not exist' => 'Not a critical error',
+        'Received signal' => 'Not a critical error',
+    ];
+  }
+
+}
